fb6bf1b063bd9914ba849087f47150d6836e9c27
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2026-01-20 00:32:16 +0800

    feat: remove Instrument Sans font family

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,11 +10,11 @@
     (function() {
       const appearance = '{{ $appearance ?? 'system' }}';
 
-      if (appearance === 'system') {
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      if (appearance === "system") {
+        const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
         if (prefersDark) {
-          document.documentElement.classList.add('dark');
+          document.documentElement.classList.add("dark");
         }
       }
     })();
@@ -37,15 +37,12 @@
   <link rel="icon" href="/favicon.svg" type="image/svg+xml">
   <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
   @viteReactRefresh
   @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
   @inertiaHead
 </head>
 
-<body class="font-sans antialiased">
+<body class="antialiased">
   @inertia
 </body>
 
