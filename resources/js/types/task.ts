6d193c25894991910f518493676add603a4bafe6
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2026-01-24 18:37:56 +0800

    feat: add optional fields

--- resources/js/types/task.ts
+++ resources/js/types/task.ts
@@ -2,6 +2,9 @@ export interface Task {
   id: number;
   title: string;
   description?: string;
+  deadline?: string;
+  priority?: "low" | "medium" | "high";
+  labels?: string[];
   completed: boolean;
   created_at: string;
   updated_at: string;
