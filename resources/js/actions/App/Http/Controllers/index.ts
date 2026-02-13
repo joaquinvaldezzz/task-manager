5a82118f6e6819be60531954cac980b2047d63eb
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2026-02-13 13:43:08 +0800

    chore: ignore action, routes, and wayfinder

--- resources/js/actions/App/Http/Controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import TaskController from './TaskController'
-import Settings from './Settings'
-
-const Controllers = {
-    TaskController: Object.assign(TaskController, TaskController),
-    Settings: Object.assign(Settings, Settings),
-}
-
-export default Controllers
